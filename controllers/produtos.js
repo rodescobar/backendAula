55511e99bc6f6fa0f0c4943f1f4b3567058ed9c6
Rodrigo Escobar <rodescobar@gmail.com>
2022-05-04 20:39:59 -0300

    Termino cadastro de produto

--- controllers/produtos.js
+++ controllers/produtos.js
@@ -8,4 +8,12 @@ module.exports = {
     Novo: async (nome, quantidade, preco, descricao, usuario) => {
         return await Produtos.create({ nome, quantidade, preco, descricao, usuario })
     },
+
+    Alterar: async (id, nome, quantidade, preco, descricao) => {
+        return await Produtos.findByIdAndUpdate(id, { nome, quantidade, preco, descricao }, { new: true})
+    },
+
+    Excluir: async (id, usuario) => {
+        return await Produtos.deleteOne({ _id: id, usuario: usuario })
+    },
 }
--- routes/produtos.js
+++ routes/produtos.js
@@ -5,7 +5,7 @@ const route = express.Router()
 const Produtos = require("../controllers/produtos")
 
 
-route.get("/produtos/listar", async (req, res) => {
+route.get("/produtos", async (req, res) => {
     const usuario = req.usuarioId
 
     var retorno = await Produtos.Listar(usuario)
@@ -13,7 +13,7 @@ route.get("/produtos/listar", async (req, res) => {
     return res.send(retorno)
 })
 
-route.post("/produtos/novo", async (req, res) => {
+route.post("/produtos", async (req, res) => {
     const { nome, quantidade, preco, descricao } = req.body
     const usuario = req.usuarioId
 
@@ -32,6 +32,40 @@ route.post("/produtos/novo", async (req, res) => {
 
     var ret = await Produtos.Novo(nome, quantidade, preco, descricao, usuario)
     return res.send(ret)
+})
+
+route.put("/produtos", async (req, res) => {
+    const { id, nome, quantidade, preco, descricao } = req.body
+    
+    if (id == "" || id == undefined)
+        return res.send({ erro: "Id não pode ser nulo." })
+
+    if (nome == "" || nome == undefined)
+        return res.send({ erro: "Nome do produto não pode ser nulo." })
+
+    if (quantidade == "" || quantidade == undefined)
+        return res.send({ erro: "Quantidade não pode ser nula." })
+
+    if (preco == "" || preco == undefined)
+        return res.send({ erro: "Preço não pode ser nulo." })
+
+    if (descricao == "" || descricao == undefined)
+        return res.send({ erro: "Descrição não pode ser nula." })
+
+    var ret = await Produtos.Alterar(id, nome, quantidade, preco, descricao)
+    return res.send(ret)
+})
+
+route.delete("/produtos", async(req, res) => {
+    const { id } = req.body
+    const usuario = req.usuarioId
+
+    if (id == "" || id == undefined)
+        return res.send({ erro: "Id não pode ser nulo." })
+
+    var ret = await Produtos.Excluir(id, usuario)
+    return res.send(ret)
+
 })
 
 module.exports = app => app.use("/app", route)
