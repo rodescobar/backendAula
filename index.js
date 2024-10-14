da7f0de525cd3e76b9e69859fb28a9ab4caf04e8
Rodrigo Escobar <rodescobar@gmail.com>
2024-10-14 16:03:02 -0300

    2024

--- index.js
+++ index.js
@@ -5,21 +5,6 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 
-
-require("./routes/usuario")(app)
-
-//MiddleWare
-const middleware = require('./auth/auth')
-app.use(middleware)
-
-
-require("./routes/produtos")(app)
-
-const port = process.env.PORT || 3000;
-app.listen(port, (req, res) => {
-    console.log("servidor on")
-})
-
 app.get('/', (req, res) => {
     res.send(`
         <!DOCTYPE html>
@@ -65,3 +50,18 @@ app.get('/', (req, res) => {
         </html>
     `);
 })
+
+require("./routes/usuario")(app)
+
+//MiddleWare
+const middleware = require('./auth/auth')
+app.use(middleware)
+
+
+require("./routes/produtos")(app)
+
+const port = process.env.PORT || 3000;
+app.listen(port, (req, res) => {
+    console.log("servidor on")
+})
+
